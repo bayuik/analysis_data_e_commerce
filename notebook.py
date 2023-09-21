f888e6b89ffe7d755d705a656fdab57d4c7042b6
bayuik <bayuindrakusuma05@gmail.com>
2023-09-21 09:29:18 +0700

    update title

--- notebook.py
+++ notebook.py
@@ -1,5 +1,5 @@
 # %% [markdown]
-# # Proyek Analisis Data: Nama dataset
+# # Proyek Analisis Data: E-Commerce Public Dataset
 # - Nama: Bayu Indra Kusuma
 # - Email: bayuindrakusuma05@gmail.com
 # - Id Dicoding: https://www.dicoding.com/users/bayuik
@@ -230,6 +230,7 @@ ax[0].set_ylabel(None)
 ax[0].set_xlabel(None)
 ax[0].set_title("By Recency (days)", loc="center", fontsize=18)
 ax[0].tick_params(axis='x', labelsize=15)
+ax[0].set_xticklabels([1, 2, 3, 4, 5])
 
 sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(
     by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
@@ -237,6 +238,7 @@ ax[1].set_ylabel(None)
 ax[1].set_xlabel(None)
 ax[1].set_title("By Frequency", loc="center", fontsize=18)
 ax[1].tick_params(axis='x', labelsize=15)
+ax[1].set_xticklabels([1, 2, 3, 4, 5])
 
 sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(
     by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
@@ -244,6 +246,7 @@ ax[2].set_ylabel(None)
 ax[2].set_xlabel(None)
 ax[2].set_title("By Monetary", loc="center", fontsize=18)
 ax[2].tick_params(axis='x', labelsize=15)
+ax[2].set_xticklabels([1, 2, 3, 4, 5])
 
 plt.suptitle("Best Customer Based on RFM Parameters (customer_id)", fontsize=20)
 plt.show()
